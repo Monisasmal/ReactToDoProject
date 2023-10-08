18bdb66d68c0dcd2887a3d1e5c317715312afe3a
ManaswiniSasmal <manisasmal5597@gmail.com>
2023-10-08 20:02:31 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React from 'react';
+// import Restaurant from './component/Basics/Restaurant';
+// import UseState from './Hooks/useState';
+// import UseEffect from './component/Hooks/useEffect';
+// import UseReducer from './component/Hooks/useReducer';
+import ToDo from './ToDoList/ToDo';
+// import Temp from './component/Weather/Temp';
+const App = () => {
+   return < ToDo/>;
+  
+};
 
 export default App;
